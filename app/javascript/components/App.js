82dfbbac34e8e178c8d85b4664850585dc98a1b8
Yair fernando <yair.facio11@gmail.com>
2020-01-12 14:02:18 -0600

    Create App component and install bootstrap, axios and styled-components

---
+++ app/javascript/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  state = {
+    films: []
+  }
+  
+  render() {
+    return(
+      <div>App</div>
+    )
+  };
+};
+
+export default App;

7fb0e7e8a355a9b8202fdaa6b31a55df73336abb
Yair fernando <yair.facio11@gmail.com>
2020-01-18 13:49:10 -0600

    Passing props to a child component using BrowserRouter

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -15,15 +15,55 @@ class App extends Component {
   //     return <Films films={this.state.films} planets={this.state.planets} people={this.state.people} />
   //   }
   // }
+  state = {
+    films: [],
+    planets: [],
+    people: []
+  }
 
+  componentDidMount = async () => {
+    const getFilms = await FilmsApi.post('/getFilms')
+    const getPlanets = await FilmsApi.post('/getPlanets')
+    const getPeople = await FilmsApi.post('/getPeoples')
+    const people = getPeople.data.contextWrites.to[0].results
+    const planets = getPlanets.data.contextWrites.to[0].results
+    const films = getFilms.data.contextWrites.to[0].results
+    console.log("MOunting")
+    this.setState({
+      films,
+      planets,
+      people
+    });
+  }
+
+  
   render() {
+    const films = (props) => {
+      return <Films films={this.state.films} planets={this.state.planets} people={this.state.people} />
+    }
+    const actors = (props) => {
+      return <Actors films={this.state.films} planets={this.state.planets} people={this.state.people} />
+    }
+    const routes = [
+      {
+        path: '/films',
+        component: films
+      },
+      {
+        path: '/actors',
+        component: actors
+      }
+    ]
     return(
       <div>
         <BrowserRouter>
           <div>
             <Header />
-            <Route path="/films" component={Films} />
-            <Route path="/actors" component={Actors} />
+            {routes.map(({ path, component: C}) =>(
+              <Route path={path} render={C} />
+            ))}
+            {/* // <Route path="/films" component={Films} />
+            // <Route path="/actors" component={Actors} /> */}
           </div>
         </BrowserRouter>
         {/* <div className="mt-5">

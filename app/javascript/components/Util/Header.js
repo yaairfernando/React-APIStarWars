6bdcbf37fe513eba344b606b2e71f8862b4494ad
Yair fernando <yair.facio11@gmail.com>
2020-01-23 23:00:10 -0600

    Adding logo

--- app/javascript/components/Util/Header.js
+++ app/javascript/components/Util/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Logo from '../img/StarWarsLogo.png'
 import { Link } from 'react-router-dom';
 
 const Container = styled.div`
@@ -80,7 +81,6 @@ const Container = styled.div`
   }
 `
 const Btn = styled.button`
-
   right: 0;
   border: none;
   background: none;
@@ -88,7 +88,7 @@ const Btn = styled.button`
   border-left: 1px solid #fff;
   height: 100%;
   margin: 0px;
-  padding: 0 37px;
+  padding: 0 87px;
   font-size: 20px;
   text-transform: uppercase;
   letter-spacing: 1.9px;
@@ -113,12 +113,11 @@ const Div = styled.div`
   flex-direction: row-reverse;
   align-items: center;
   height: 100%;
-  
+  width: 241px;
   right: 15px;
   display: flex;
   border-left: 1px solid #fff;
   padding: 20px;
-  margin: 0 5px;
 
   & > div.userInfo{
     display: flex;
@@ -184,8 +183,8 @@ const Header = (props) => {
           <a className="" href="#">Navbar</a>
           <div className="links-container">
             <Link to="/films" >Films</Link>
-            <Link to="/" >
-              <h1>Star Wars</h1>
+            <Link to="/" className="w-100">
+              <img src={Logo} className="w-25" />
             </Link> 
             <Link to="/species" >Species</Link>
           </div>
@@ -200,39 +199,3 @@ const Header = (props) => {
 };
 
 export default Header;
-
-{/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav> */}
